82e35803e7aff442eb2cd487d140da892310c12a
Larzid <juanitocampamocha@gmail.com>
2016-10-12 01:03:27 -0500

    Fix ai.PlayerControlled loop.

--- ai.py
+++ ai.py
@@ -4,8 +4,8 @@ import render
 import get_input
 
 class BasicMonster:
-  def __init__(self, action=None):
-    self.action = action
+  def __init__(self):
+    self.action = ''
   def take_turn(self):
     monster = self.owner
     globals.fov_recompute(monster)
@@ -24,23 +24,23 @@ class BasicMonster:
       elif target.fighter.hp > 0:
         monster.fighter.attack(target)
     else:
-      movement = False
-      while movement is False:
+      self.action = 'didnt-take-turn'
+      while self.action == 'didnt-take-turn':
         (x, y) = (libtcod.random_get_int(0, -1, 1), libtcod.random_get_int(0, -1, 1))
         if not globals.is_blocked(monster.x + x, monster.y + y):
-          movement = True
+          self.action = 'moved'
           monster.move(x, y)
 
 class PlayerControlled:
-  def __init__(self, action=None):
-    self.action = action
+  def __init__(self):
+    self.action = ''
   def take_turn(self):
     self.action = 'didnt-take-turn'
-    render.all(self.owner)
-    libtcod.console_flush()
     while self.action == 'didnt-take-turn':
+      render.all(self.owner)
+      libtcod.console_flush()
       self.action = get_input.handle_keys(self.owner)
-    for object in globals.objects():
-      render.clear(object)
-    if self.owner.fighter:
-      self.owner.fighter.check_status = True
+      for object in globals.objects():
+        render.clear(object)
+      if self.owner.fighter:
+        self.owner.fighter.check_status = True
